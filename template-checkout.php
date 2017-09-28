c1e2059edb705a0d6241b684fde4cf81b3349623
SolalDR <solal.dussout-revel@hotmail.fr>
2017-09-28 08:23:58 +0200

    Update CSS

--- template-checkout.php
+++ template-checkout.php
@@ -8,13 +8,13 @@ get_header(); ?>
 <main id="page" class="site-main checkout-page" role="main">
   <div class="landing" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
     <div class="container">
-			<?php
-			while ( have_posts() ) : the_post(); ?>
-			<h1 class="left-full-border"><?php echo get_the_title(); ?></h1>
-			<div class="single-body">
-				<?php the_content(); ?>
-			</div>
-			<?php endwhile; // End of the loop. ?>
+		<?php
+		while ( have_posts() ) : the_post(); ?>
+		<h1 class="left-full-border"><?php echo get_the_title(); ?></h1>
+		<div class="single-body">
+			<?php the_content(); ?>
+		</div>
+		<?php endwhile; // End of the loop. ?>
     </div>
   </div>
 </main>
