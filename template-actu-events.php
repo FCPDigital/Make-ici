e93770c7d686b040e532ab3b567fca103998af26
SolalDR <solal.dussout-revel@hotmail.fr>
2017-10-02 09:11:36 -0700

    Start integrate actu-event

---
+++ template-actu-events.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Template Name: Actualité / Events
+ */
+
+get_header(); ?>
+
+<main id="page" class="site-main actu-events" role="main" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)">
+	<div class="wrapper container">
+		<div class="flex-big main-part">
+			<h1 class="main-title">Actualités</h1>
+			
+			<div class="items">
+
+				<div class="item item--big">
+					<div class="item__banner">
+						<p class="item__title item__title--top-left">Incubateur de Made in france</p>
+						<span class="item__corner item__corner--bottom-right">20 juin 2017</span>
+					</div>
+					<div class="item__thumbnail">
+						<img src="http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/05/yoda-advice-always-in-motion-is-the-future.jpg" alt="">
+					</div>
+					<div class="item__content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus assumenda optio esse eaque rem veniam et nostrum, distinctio animi. Dolorum a atque distinctio molestiae pariatur neque inventore quod cum, reprehenderit!</p>
+					</div>
+				</div>
+				
+				<div class="item item--small">
+					<div class="item__banner">
+						<p class="item__title item__title--top-left">Incubateur de Made in france</p>
+						<span class="item__corner item__corner--bottom-right">20 juin 2017</span>
+					</div>
+					<div class="item__thumbnail">
+						<img src="http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/05/yoda-advice-always-in-motion-is-the-future.jpg" alt="">
+					</div>
+					<div class="item__content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus assumenda optio esse eaque rem veniam et nostrum, distinctio animi. Dolorum a atque distinctio molestiae pariatur neque inventore quod cum, reprehenderit!</p>
+					</div>
+				</div>
+
+				<div class="item item--small">
+					<div class="item__banner">
+						<p class="item__title item__title--top-left">Incubateur de Made in france</p>
+						<span class="item__corner item__corner--bottom-right">20 juin 2017</span>
+					</div>
+					<div class="item__thumbnail">
+						<img src="http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/05/yoda-advice-always-in-motion-is-the-future.jpg" alt="">
+					</div>
+					<div class="item__content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus assumenda optio esse eaque rem veniam et nostrum, distinctio animi. Dolorum a atque distinctio molestiae pariatur neque inventore quod cum, reprehenderit!</p>
+					</div>
+				</div>
+
+				<div class="item item--big">
+					<div class="item__banner">
+						<p class="item__title item__title--top-left">Incubateur de Made in france</p>
+						<span class="item__corner item__corner--bottom-right">20 juin 2017</span>
+					</div>
+					<div class="item__thumbnail">
+						<img src="http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/05/yoda-advice-always-in-motion-is-the-future.jpg" alt="">
+					</div>
+					<div class="item__content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus assumenda optio esse eaque rem veniam et nostrum, distinctio animi. Dolorum a atque distinctio molestiae pariatur neque inventore quod cum, reprehenderit!</p>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+		<div class="flex-small second-part">
+			<h2 class="main-title">Evenements</h2>
+		</div>
+	</div>
+</main>
+
+<?php get_footer();
