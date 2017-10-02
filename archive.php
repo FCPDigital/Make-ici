c6f14a4352f622fd1a18120a6016e1c9674d9b43
SolalDR <solal.dussout-revel@hotmail.fr>
2017-10-02 03:34:53 -0700

    Update archive.php

--- archive.php
+++ archive.php
@@ -23,66 +23,8 @@
 					<a href="#" class="carousel-control-btn" data-direction="right"></a>
 				</div>
 			</div>
-
 		</div>
 	</div>
-	<div class="body loop-archive" id="looper-snap">
-		<div id="scroll-container">
-			<?php
-			if ( have_posts() ) : ?>
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
-				?>
-				<section id="item-<?php echo get_the_slug(); ?>" class="no-sidebar archive-body-item awesome-panel-item container-fluid page-section" style="background-image:url('<?php echo get_the_post_thumbnail_url(); ?>')">
-					<div class="container">
-						<div class="archive-main">
-							<div class="archive-main-head">
-								<h2 class="title">
-									<?php echo get_the_title(); ?>
-								</h2>
-								<h3>
-									<?php echo get_field("subtitle"); ?>
-								</h3>
-							</div>
-							<div class="archive-main-body">
-								<?php echo get_the_content(); ?>
-							</div>
-						</div>
-					</div>
-				</section>
-				<?php
-				endwhile;
-
-				the_posts_pagination( array(
-					'prev_text' => '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>',
-					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-				) );
-
-			else :
-
-				get_template_part( 'template-parts/post/content', 'none' );
-
-			endif; ?>
-		</div>
-
-	</div>
-
-	<div class="timeline hide-state">
-		<?php
-		if ( have_posts() ) :	/* Start the Loop */
-			while ( have_posts() ) : the_post(); ?>
-
-			<a class="timeline-item" data-target="#item-<?php echo get_the_slug(); ?>" href="#item-<?php echo get_the_slug(); ?>">
-				<?php echo get_the_title(); ?>
-			</a>
-
-			<?php endwhile;
-		endif; ?>
-		<span class="active-point"></span>
-	</div>
-
 </main>
 
 <?php get_footer();
