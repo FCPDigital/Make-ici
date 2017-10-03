60320f8c13636aa8a1b4e573d7be8e407729b8a1
SolalDR <solal.dussout-revel@hotmail.fr>
2017-10-03 08:55:44 -0700

    Try fixe bug get_field  empty in abonnement

--- template-parts/post/content-abonnements.php
+++ template-parts/post/content-abonnements.php
@@ -27,16 +27,20 @@ Ce template est appelé notamment dans archive-abonnement.php
 
 		<?php // Sidebar de droite ?>
 		<div class="archive-sidebar">
-
+			
+			<?php if (!get_field("cost_per_month") || get_field("cost_per_month") == "") { ?>
 			<div class="cost_per_month">
 				<i class="fa fa-eur" aria-hidden="true"></i>
 				<p>Coût mensuel :<br><?php echo get_field("cost_per_month"); ?></p>
 			</div>
-
+			<?php } ?>
+			
+			<?php if (!get_field("cost_registration") || get_field("cost_registration") == "") { ?>
 			<div class="cost_registration">
 				<i class="fa fa-inscription" aria-hidden="true"></i>
 				<p>Frais d'inscription :<br><?php echo get_field("cost_registration"); ?></p>
 			</div>
+			<?php } ?>
 
 			<p class="mention">Durée minimale :<br><?php echo get_field("duration_min");	?> / Préavis <?php echo get_field("duration_preavis");	?></p>
 			<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
