2a66a2c4a9cee4fd7b37c82ff5afdfb939b44c15
SolalDR <solal.dussout-revel@hotmail.fr>
2017-10-06 06:03:07 -0700

    Integrate 404.php

--- 404.php
+++ 404.php
@@ -1,34 +1,21 @@
-<?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<main id="page" class="site-main homepage" role="main">
+	<div class="landing vertical-center" style="background-image:url(http://makeici.org/icimontreuil/wp-content/uploads/sites/2/2017/05/home-ici-montreuil.jpg);">	 
+	 	<div class="container-404 container">
+	 		<p class="title">Erreur</p>
+			<div class="code">
+				<span class="code__letter">4</span>
+				<img class="code__anim" src="<?php echo get_template_directory_uri().'/assets/images/circular-saw.png'; ?>" alt="">
+				<span class="code__letter">4</span>
+			</div>
+			<p class="code__message">La page que vous recherchez n'existe pas.</p>
+			<div class="center margin-top-medium">
+				<a href="<?php 	echo get_site_url(); ?>" class="btn btn--light">Revenir à l'accueil</a>
+			</div>
+		</div>
+	</div>
+</main>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyseventeen' ); ?></h1>
-				</header><!-- .page-header -->
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyseventeen' ); ?></p>
-
-					<?php get_search_form(); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
-
-<?php get_footer();
+<?php get_footer();?>
