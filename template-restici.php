7aa68d9a2ee6e40fbcf17bf36028e0174d893bb5
SolalDR <solal.dussout-revel@hotmail.fr>
2017-10-10 04:06:22 -0700

    Reindente makeici

--- template-restici.php
+++ template-restici.php
@@ -12,15 +12,13 @@ get_header(); ?>
 				<?php //Contenu principale de la section ?>
 				<div class="col-sm-8">
 					<div class="archive-main-head">
-			<h1 class="left-full-border"><?php echo get_the_title(); ?></h1>
-			<div class="single-body">
-			<p>
-			<?php
-			while ( have_posts() ) : the_post(); ?>
-			<?php the_content(); ?></p>
-			<?php endwhile; // End of the loop. ?>
-			</div>
-			</div>		 
+						<h1 class="left-full-border"><?php echo get_the_title(); ?></h1>
+						<div class="single-body margin-bottom-medium">
+							<p><?php while ( have_posts() ) : the_post(); ?>
+							<?php the_content(); ?></p>
+							<?php endwhile; // End of the loop. ?>
+						</div>
+					</div>		 
 				</div>
 				<?php //Sidebar de droite ?>
 				<div class="col-sm-4 ">
@@ -28,25 +26,19 @@ get_header(); ?>
 						<p class="rest-img-content"><?php echo get_field("texte_chef"); ?><p>&nbsp;</p></p>
 					</div>
 				</div>
-					
-					
-					
-			<div class="clr"></div>
+				<div class="clr"></div>
+			</div>
+			<a class="scroll-btn" href="#anchor-2" data-scroll></a>
 		</div>
-		<a class="scroll-btn" href="#anchor-2" data-scroll></a>
 	</div>
-		</div>
-	
 	
 	<div id="anchor-2" class="section-post" style="background-image: url(<?php echo get_field('fond_rest-ici_formules'); ?>);">
 		<div class="container">
-		
 			<h2 class="left-full-border">3 Formules, tous les midis</h2>
-			<div class="single-body">
+			<div class="single-body margin-bottom-medium">
 				<div id="main-carousel" class="carousel ">
 					<div class="carousel-body rest">
 						<div class="archive-head carousel-container">
-
 							<div class="item  item--height-auto archive-head-item carousel-item">
 								<div class="item__banner">
 									<p class="item__title">SUR PLACE</p>
@@ -87,46 +79,42 @@ get_header(); ?>
 							</div>
 						</div>
 					</div>
+				</div>
 			</div>
+			<a class="scroll-btn" href="#anchor-3" data-scroll></a>
 		</div>
-		<a class="scroll-btn" href="#anchor-3" data-scroll></a>
-	</div>
- </div>
-	 <div id="anchor-3" class="section-post last-posts" style="background-image: url(<?php echo get_field("fond_formule"); ?>)">
+ 	</div>
+	
+	<div id="anchor-3" class="section-post last-posts" style="background-image: url(<?php echo get_field("fond_formule"); ?>)">
 		<div class="container">
 			<div class="archive-main row">
 				<?php //Contenu principale de la section ?>
 				<div class="col-sm-8">
 					<div class="archive-main-head">
-				<h2 class="left-full-border">Formule</h2>
-				 <p>&nbsp;</p>
-			<div class="single-body">
-			<p><?php echo get_field("texte_formule"); ?> </p>
-			</div>
-			</div>		 
+						<h2 class="left-full-border  margin-bottom-medium">Formule</h2>
+						<div class="single-body margin-bottom-medium">
+							<p><?php echo get_field("texte_formule"); ?> </p>
+						</div>
+					</div>		 
 				</div>
 				<?php //Sidebar de droite ?>
 				<div class="col-sm-4 ">
 					<div class="rest-img"><img src="<?php echo get_field("photo_manager"); ?>" width="304" alt="">
 						<p class="rest-img-content"><?php echo get_field("texte_manager"); ?></p>
 					</div>
-					</div>
-			
-			<div class="clr"></div>
+				</div>
+				<div class="clr"></div>
+			</div>
+			<a class="scroll-btn" href="#anchor-4" data-scroll></a>
 		</div>
-		<a class="scroll-btn" href="#anchor-4" data-scroll></a>
-	</div>
 	 </div>
+
 	 <div id="anchor-4" class="section-post" style="background-image: url(<?php echo get_field('slider_fond'); ?>);">
 		<div class="container">
-		
-			<h2 class="left-full-border">Photos</h2>
-			 <p>&nbsp;</p>
-			<div class="single-body"><?php echo get_field("photo-slider"); ?>
-			</div>
-			</div>
+			<h2 class="left-full-border margin-bottom-medium">Photos</h2>
+			<div class="single-body margin-bottom-medium"><?php echo get_field("photo-slider"); ?></div>
 		</div>
-				</div>
+	</div>
 </main>
 
 <?php get_footer();
