bcd715276053002832115c6bce7bd8fb42145a20
SolalDR <solal.dussout-revel@hotmail.fr>
2018-07-05 06:56:39 -0700

    Add acf field to resident

--- helpers/acf_field.php
+++ helpers/acf_field.php
@@ -1,6 +1,9 @@
 <?php 
 if(function_exists("register_field_group"))
 {
+	/****************************************************
+			Modèle de page : Page Formulaire
+	*****************************************************/
 	register_field_group(array (
 		'id' => 'acf_page-formulaire',
 		'title' => 'Page formulaire',
@@ -57,6 +60,48 @@ if(function_exists("register_field_group"))
 					'param' => 'page_template',
 					'operator' => '==',
 					'value' => 'template-page-form.php',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+
+	/****************************************************
+						Résidents
+	*****************************************************/
+	register_field_group(array (
+		'id' => 'acf_residents',
+		'title' => 'Résidents',
+		'fields' => array (
+			array (
+				'key' => 'field_5b3cddf9a3ff9',
+				'label' => 'Savoirs faires',
+				'name' => 'savoirs_faires',
+				'type' => 'select',
+				'choices' => array (
+					'bois' => 'Bois',
+					'metal' => 'Métal',
+					'numeric' => 'Numérique',
+				),
+				'default_value' => '',
+				'allow_null' => 0,
+				'multiple' => 0,
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'residents',
 					'order_no' => 0,
 					'group_no' => 0,
 				),
