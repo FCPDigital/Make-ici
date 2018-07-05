c675ab0ffcb90a3986738654a63542390a47d5cc
SolalDR <solal.dussout-revel@hotmail.fr>
2018-07-05 06:38:17 -0700

    Improve archive-residents.php

--- archive-residents.php
+++ archive-residents.php
@@ -1,16 +1,59 @@
 <?php get_header(); ?>
 
+<?php
+if ( function_exists( 'get_sites' )) {
+	$original_blog_id = get_current_blog_id();
+	$blog_list = get_sites( 0, 'all' );
+	$residents = [];
+	$residents_blogs = [];
+	$residents_url = [];
+	$blogs_info = [];
+	foreach ($blog_list AS $blog) {
+		switch_to_blog( (int) $blog->blog_id );
 
+		var_dump($blog);
+		if($blog->deleted == 0 && $blog->path != "/"){
+			array_push($blogs_info, array(
+					"name" => get_bloginfo( 'name' ),
+					"path" => $blog->path,
+					"id" => $blog->blog_id
+			));
+		}
+
+
+		$posts = get_posts(array(
+			'post_type'  => 'residents',
+			'posts_per_page'=> -1,
+			'post_status'=>'publish'
+		));
+		if(is_array($posts)){
+			for($i=0; $i<count($posts); $i++){
+				array_push($residents, $posts[$i]);
+				array_push($residents_blogs, $blog);
+				array_push($residents_url, get_post_permalink($posts[$i]));
+			}
+		}
+	}
+	restore_current_blog();
+} else {
+	$residents = get_posts(array(
+		'post_type'  => 'residents',
+		'posts_per_page'=> -1,
+		'post_status'=>'publish'
+	));
+}
+?>
 <main id="actus" class="site-main actus landing" role="main" style="background-image: url(http://makeici.org/icimontreuil/wp-content/uploads/sites/2/2017/05/home-ici-montreuil.jpg);">
 	<div id="residents-filter" class="wrapper container">
-		<h1 class="bold text-center margin-bottom-medium">Nos résidents</h1>
+		<h1 class="text-center bold">Nos résidents</h1>
 		<div class="filter">
-			<div class="filter__list text-center margin-bottom-big">
+			<div class="filter__list text-center">
+				<p>Filtres : </p>
 				<select class="select select--light" name="blog" data-filters='{"modifier": "hidden", "value": false, "order": 1}'>
 					<option value="false">Tous</option>
-					<option value="">ICI Montreuil</option>
-					<option value="">ICI Marseille</option>
-					<option value="">ICI TheCamp</option>
+					<?php foreach($blogs_info as $key => $infos) { ?>
+						<option value="<?php echo $infos["id"]; ?>"><?php echo $infos["name"]; ?></option>
+					<?php } ?>
 				</select>
 				<select class="select select--light" name="savoirs_faires" data-filters='{"modifier": "hidden", "value": false, "order": 2}'>
 					<option value="false">Tous</option>
@@ -22,35 +65,6 @@
 		</div>
 		<div id="masonry-resident">
 			<?php
-			if ( function_exists( 'get_sites' )) {
-				$original_blog_id = get_current_blog_id();
-				$blog_list = get_sites( 0, 'all' );
-				$residents = [];
-				$residents_blogs = [];
-				$residents_url = [];
-				foreach ($blog_list AS $blog) {
-					switch_to_blog( (int) $blog->blog_id );
-					$posts = get_posts(array(
-						'post_type'  => 'residents',
-						'posts_per_page'=> -1,
-						'post_status'=>'publish'
-					));
-					if(is_array($posts)){
-						for($i=0; $i<count($posts); $i++){
-							array_push($residents, $posts[$i]);
-							array_push($residents_blogs, $blog);
-							array_push($residents_url, get_post_permalink($posts[$i]));
-						}
-					}
-				}
-				restore_current_blog();
-			} else {
-				$residents = get_posts(array(
-					'post_type'  => 'residents',
-					'posts_per_page'=> -1,
-					'post_status'=>'publish'
-				));
-			}
 
 			for($i=0; $i<count($residents); $i++){
 				$post = $residents[$i];
